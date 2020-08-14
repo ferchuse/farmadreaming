b5791d5101fd44826d13f5e3b84637cc5c0db94f
ferchuse <sistemas@glifo.mx>
2020-08-14 13:50:35 -0500

    login redirect

--- login/logout.php
+++ login/logout.php
@@ -1,5 +1,5 @@
 <?php 
 	session_start();	session_destroy();		setcookie("id_usuarios", "",  0, "/");	setcookie("permiso_usuarios", "",  0, "/");	setcookie("nombre_usuarios", "",  0, "/");
 	setcookie("id_sucursal", "",  0, "/");
-	// setcookie("nombre_usuarios", "",  0, "/");		header("location:main_login.php");	
+	// setcookie("nombre_usuarios", "",  0, "/");		header("location:index.php");	
 ?>
