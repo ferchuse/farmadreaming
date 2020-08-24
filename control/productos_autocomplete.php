75dfb7e0253b4ef12a183a8c4e6e3f5bce19b89f
ferchuse <sistemas@glifo.mx>
2020-08-24 17:55:55 -0500

    Update productos_autocomplete.php

--- control/productos_autocomplete.php
+++ control/productos_autocomplete.php
@@ -12,26 +12,10 @@
 	
 	$consulta = "SELECT * FROM $tabla 
 	
-	LEFT JOIN sucursal_existencias
-	USING(id_productos)
-	
-	LEFT JOIN 
-	(
-	SELECT id_productos, existencia AS san_sebas 
-	FROM sucursal_existencias WHERE id_sucursal = 1
-	) AS t_san_sebas
-	USING(id_productos)
-	
-	LEFT JOIN 
-	(
-	SELECT id_productos, existencia AS zumpango 
-	FROM sucursal_existencias WHERE id_sucursal = 2
-	) AS t_zumpango
-	USING(id_productos)
 	
 	
 	WHERE $campo LIKE '%$query%' 
-	AND id_sucursal = '{$_COOKIE["id_sucursal"]}'
+	
 	
 	
 	ORDER BY $campo LIMIT 50 ";
